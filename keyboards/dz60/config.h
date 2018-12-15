0ef2e8d4a4e4211cf89660bdafdbcc3ce3dcd1b3
slidebox <45889193+slidebox@users.noreply.github.com>
2018-12-14 21:56:52 -0500

    Update config.h

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -28,7 +28,7 @@
 #define BACKLIGHT_LEVELS 5
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCING_DELAY 1
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
